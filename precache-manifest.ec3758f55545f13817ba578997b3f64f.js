c9019c418bef2ba77e9eb73f618698fb39baa8ca
Sanchit Sharma <sanchit.sharma@olx.com>
2023-01-24 19:48:39 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.ec3758f55545f13817ba578997b3f64f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e434f0ff0636eef07b823e56808a706f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "f77f235211bea6121348",
+    "url": "/react-gh-pages/static/css/2.5c572189.chunk.css"
+  },
+  {
+    "revision": "5c3d711ff949667019a3",
+    "url": "/react-gh-pages/static/css/main.daf578ef.chunk.css"
+  },
+  {
+    "revision": "f77f235211bea6121348",
+    "url": "/react-gh-pages/static/js/2.7bba826d.chunk.js"
+  },
+  {
+    "revision": "d7b6aa3216a46e158676000aa53481ad",
+    "url": "/react-gh-pages/static/js/2.7bba826d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3d711ff949667019a3",
+    "url": "/react-gh-pages/static/js/main.d528920a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-gh-pages/static/js/main.d528920a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774f2d71074175359368",
+    "url": "/react-gh-pages/static/js/runtime-main.467c293f.js"
+  },
+  {
+    "revision": "e2a914671640c420a53efc6c41a5b288",
+    "url": "/react-gh-pages/static/media/imattsmart-kigHD-rVJzA-unsplash.e2a91467.jpg"
+  }
+]);
